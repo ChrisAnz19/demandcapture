c155126cd99b342e5c49bbc8af69c69c006cdb96
Hyzyr <avezberdiyew@gmail.com>
2024-01-03 23:18:55 +0500

    fixis

--- src/pages/OurProcess/OurProcess.jsx
+++ src/pages/OurProcess/OurProcess.jsx
@@ -3,8 +3,7 @@ import OurProcessHero from "./sections/OurProcessHero";
 import Onboarding from "./sections/Onboarding";
 
 import { useHeaderStyles } from "layouts/header";
-import Testimonials from "layouts/testimonials/Testimonials";
-import { FAQ } from "layouts";
+import { FAQ, Testimonials } from "layouts";
 
 import "./styles.scss";
 
